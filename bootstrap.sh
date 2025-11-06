c3aa7d3de9d51c87904a2a5911c18c4c7b2f302f
Evan Duong <evduong@icloud.com>
2025-11-06 16:06:14 +0700

    Add bootstrap script for Debian/Ubuntu servers

---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Enable strict error handling
+set -Eeuo pipefail
+
+# Require root privileges
+[[ $EUID -eq 0 ]]
+
+# Configure timezone
+timedatectl set-timezone UTC
+
+# Prevent DHCP from overwriting DNS settings
+install -Dm755 /dev/stdin /etc/dhcp/dhclient-enter-hooks.d/nodns <<EOF
+make_resolv_conf() { :; }
+EOF
+
+# Update and upgrade packages
+apt update && apt upgrade
+
+# Install packages
+apt install curl ufw ssh-import-id qemu-guest-agent
+
+# Install Docker
+curl -fsSL https://get.docker.com | sh
+
+# Install and configure Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh && tailscale up
+
+# Remove unused packages and cache
+apt autoremove --purge && apt clean
+
+# Set up new user
+adduser --gecos -- runner && usermod -aG sudo,docker $_ && runuser -u $_ -- ssh-import-id-gh evandn
+
+# Disable root password
+passwd -dl $USER
+
+# Remove residual files
+find $HOME -mindepth 1 ! -name .bashrc ! -name .profile -exec rm -rf {} +
+
+# Configure SSH server
+install -Dm600 /dev/stdin /etc/ssh/sshd_config <<EOF && sshd -t && systemctl restart ssh
+AllowUsers runner
+PermitRootLogin no
+PasswordAuthentication no
+KbdInteractiveAuthentication no
+UsePAM yes
+PrintMotd no
+EOF
+
+# Configure firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow in on tailscale0
+ufw allow http
+ufw allow https
+ufw allow 41641/udp
+ufw enable
