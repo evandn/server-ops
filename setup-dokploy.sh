fabc6b06933087e270c47d04c4e902b9c0a25522
Evan Duong <evduong@icloud.com>
2026-05-30 21:20:04 +0700

    Refactor Dokploy installation script to streamline setup and enhance Tailscale integration

--- setup-dokploy.sh
+++ setup-dokploy.sh
@@ -6,20 +6,8 @@ set -Eeuxo pipefail
 # Require root privileges
 [[ $EUID -eq 0 ]]
 
-# Get Tailscale IPv4
-ip=$(tailscale ip -4)
+# Install Dokploy without exposing ports
+curl -fsSL https://dokploy.com/install.sh | sed '/--publish\|-p 443/d; s/80:80/127.0.0.1:&/' | ADVERTISE_ADDR=$(tailscale ip -4) bash
 
-# Install and set up Dokploy behind Tailscale
-curl -fsSL https://dokploy.com/install.sh | sed \
-  -e '/--publish/d' \
-  -e '/docker.sock:ro/a\        -v /var/run/tailscale/tailscaled.sock:/var/run/tailscale/tailscaled.sock:ro \\' |
-  ADVERTISE_ADDR=$ip bash
-
-# Get Tailscale FQDN
-url=$(tailscale whois $ip | grep -o '[^ ]*\.ts\.net')
-
-# Bind Dokploy dashboard to Tailscale FQDN
-sed -i "s/\`.*\`/\`$url\`/" /etc/dokploy/traefik/dynamic/dokploy.yml
-
-# Add Tailscale certificate resolver to Traefik
-sed -i '/certificatesResolvers:/a\  tailscale:\n    tailscale: {}' /etc/dokploy/traefik/traefik.yml
+# Serve Dokploy dashboard over Tailscale
+tailscale serve --bg 80
